ba1015de2d2125453490e59c820db444f48a5ba5
Flyiva_01 <mohamednjikam25@hotmail.com>
2021-04-24 22:34:19 +0300

    Done

--- train_model.bat
+++ train_model.bat
@@ -2,19 +2,5 @@
 
 SET model_name=Ensemble
 SET mode=ensemble
-SET ensemble_size=15
+SET ensemble_size=1
 python train.py --MODEL_PATH trained\%mode%/%model_name% --MODEL_NAME 1 --DOWN_DROP 0,0,0,0 --UP_DROP 0,0,0,0
-python train.py --MODEL_PATH trained\%mode%/%model_name% --MODEL_NAME 2 --DOWN_DROP 0,0,0,0 --UP_DROP 0,0,0,0
-python train.py --MODEL_PATH trained\%mode%/%model_name% --MODEL_NAME 3 --DOWN_DROP 0,0,0,0 --UP_DROP 0,0,0,0
-python train.py --MODEL_PATH trained\%mode%/%model_name% --MODEL_NAME 4 --DOWN_DROP 0,0,0,0 --UP_DROP 0,0,0,0
-python train.py --MODEL_PATH trained\%mode%/%model_name% --MODEL_NAME 5 --DOWN_DROP 0,0,0,0 --UP_DROP 0,0,0,0
-python train.py --MODEL_PATH trained\%mode%/%model_name% --MODEL_NAME 6 --DOWN_DROP 0,0,0,0 --UP_DROP 0,0,0,0
-python train.py --MODEL_PATH trained\%mode%/%model_name% --MODEL_NAME 7 --DOWN_DROP 0,0,0,0 --UP_DROP 0,0,0,0
-python train.py --MODEL_PATH trained\%mode%/%model_name% --MODEL_NAME 8 --DOWN_DROP 0,0,0,0 --UP_DROP 0,0,0,0
-python train.py --MODEL_PATH trained\%mode%/%model_name% --MODEL_NAME 9 --DOWN_DROP 0,0,0,0 --UP_DROP 0,0,0,0
-python train.py --MODEL_PATH trained\%mode%/%model_name% --MODEL_NAME 10 --DOWN_DROP 0,0,0,0 --UP_DROP 0,0,0,0
-python train.py --MODEL_PATH trained\%mode%/%model_name% --MODEL_NAME 11 --DOWN_DROP 0,0,0,0 --UP_DROP 0,0,0,0
-python train.py --MODEL_PATH trained\%mode%/%model_name% --MODEL_NAME 12 --DOWN_DROP 0,0,0,0 --UP_DROP 0,0,0,0
-python train.py --MODEL_PATH trained\%mode%/%model_name% --MODEL_NAME 13 --DOWN_DROP 0,0,0,0 --UP_DROP 0,0,0,0
-python train.py --MODEL_PATH trained\%mode%/%model_name% --MODEL_NAME 14 --DOWN_DROP 0,0,0,0 --UP_DROP 0,0,0,0
-python train.py --MODEL_PATH trained\%mode%/%model_name% --MODEL_NAME 15 --DOWN_DROP 0,0,0,0 --UP_DROP 0,0,0,0
